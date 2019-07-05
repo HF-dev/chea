82fcd61339e40bbcd2647f04a9749fdac117ed73
thesavoyard <nathan.gb73@gmail.com>
2019-07-05 17:31:04 +0200

    fghujik

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,8 +9,15 @@ export class MapComponent implements OnInit {
   public lat: number = 51.678418;
   public lng: number = 7.809007;
   
-  constructor() { }
-
+  constructor() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=>{
+        this.lat=position.coords.latitude;
+        this.lng=position.coords.longitude;
+      })
+    }
+   }
+ 
   ngOnInit() {
   }
 
